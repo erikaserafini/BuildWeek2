782515b94b1448840c8ba5f6cf81cfb18c4f28cb
EleonoraMassiglia <167798814+EleonoraMassiglia@users.noreply.github.com>
2024-07-18 21:26:55 +0200

    Pagina 1
    da implementare logica

---
+++ Buildweek2 pagina 1/script.js
@@ -0,0 +1,15 @@
+const locations = [
+    Genova, Milano, Inverness, Montecarlo, Edimburgh, Kirkwall, Rapallo, Nordkapp,
+]
+
+
+const location = document.querySelector(".location")
+const date =document.querySelector(".Check-in")
+const expDate = document.querySelector(".Check-out")
+const guest = document.querySelector(".guest")
+const btn = document.getElementById("btn")
+
+
+btn.addEventListener("click" )
+
+
